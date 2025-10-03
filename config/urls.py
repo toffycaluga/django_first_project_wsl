f98969e32c367fe48250cc4285a57b25e9b9feac
Abraham Lillo <abraham.lillol@gmail.com>
2025-10-03 17:33:43 -0300

    doc: agregando requerimientos

--- config/urls.py
+++ config/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
-
+from reports.views import informe_view
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='index.html'),name='home'),
